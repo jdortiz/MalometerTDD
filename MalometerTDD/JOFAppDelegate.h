628d64ee8101b1286f300f5c7d7d61d940dbb4d1
Jorge D. Ortiz-Fuentes <jdortiz@gmail.com>
2014-06-08 22:54:15 +0200

    Initial Commit

---
+++ MalometerTDD/JOFAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  JOFAppDelegate.h
+//  MalometerTDD
+//
+//  Created by Jorge D. Ortiz Fuentes on 08/06/14.
+//  Copyright (c) 2014 PoWWaU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JOFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ MalometerTDD/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MalometerTDD
+//
+//  Created by Jorge D. Ortiz Fuentes on 08/06/14.
+//  Copyright (c) 2014 PoWWaU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JOFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JOFAppDelegate class]));
+    }
+}
