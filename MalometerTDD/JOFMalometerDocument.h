98ed8b39dd82a0ecd39537f86654e4ee129780db
Jorge D. Ortiz-Fuentes <jdortiz@gmail.com>
2014-06-28 13:34:59 +0200

    Added the first test of the MalometerDocument.

---
+++ MalometerTDD/JOFMalometerDocument.h
@@ -0,0 +1,13 @@
+//
+//  JOFMalometerDocument.h
+//  MalometerTDD
+//
+//  Created by Jorge D. Ortiz Fuentes on 28/06/14.
+//  Copyright (c) 2014 PoWWaU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JOFMalometerDocument : UIManagedDocument
+
+@end
